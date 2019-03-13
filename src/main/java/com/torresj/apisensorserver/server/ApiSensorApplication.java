a855fc468fe6ba872e65932404454f6ad7033ec1
Jaime Torres <jtbenavente@gmail.com>
2019-03-13 19:08:20 +0100

    New structure

---
+++ src/main/java/com/torresj/apisensorserver/server/ApiSensorApplication.java
@@ -0,0 +1,14 @@
+package com.torresj.apisensorserver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSensorApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/torresj/apisensorserver/server/ApiSensorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.torresj.apisensorserver.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiSensorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
