5a48310645c034152654f0b5c8c63951978e7085
Jaime Torres <jatorres@MALLM-JATORRES.local>
2019-08-07 16:51:24 +0200

    User controller log updated

--- src/main/java/com/torresj/apisensorserver/controller/UserController.java
+++ src/main/java/com/torresj/apisensorserver/controller/UserController.java
@@ -50,23 +50,28 @@ public class UserController {
   public ResponseEntity<Page<User>> getUsers(@RequestParam(value = "page") int nPage,
       @RequestParam(value = "elements") int elements, Principal principal) {
     try {
-      logger.info("[USER - GET ALL] Check user permission");
+      logger
+          .info("[USER - GET ALL] Getting users with page {}, elements {} by user \"{}\"", nPage,
+              elements, principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger
-          .info("[USER - GET ALL] Get users from DB with page " + nPage + ", elements " + elements);
 
       Page<User> page = userService.getUsers(nPage, elements);
 
+      logger
+          .info(
+              "[USER - GET ALL] Request for getting users with page {}, elements {} finished by user \"{}\"",
+              nPage,
+              elements, principal.getName());
       return new ResponseEntity<>(page, HttpStatus.OK);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - GET ALL] user Not have permission for this endpoint");
+      logger.error("[USER - GET ALL] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER - GET ALL] Error getting users from DB", e);
+      logger.error("[USER - GET ALL] Error getting users", e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -75,26 +80,27 @@ public class UserController {
   @ApiOperation(value = "Retrieve user by id", response = User.class)
   public ResponseEntity<User> getUserByID(@PathVariable("id") long id, Principal principal) {
     try {
-      logger.info("[USER - GET] Check user permission");
+      logger.info("[USER - GET] Getting user {} by user \"{}\"", id, principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN) && !userService
           .isSameUser(principal.getName(), id)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER - GET] Get user from DB with id: " + id);
 
       User user = userService.getUser(id);
 
+      logger.info("[USER - GET] Request getting user {} finished by user \"{}\"", id,
+          principal.getName());
       return new ResponseEntity<>(user, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
       logger.error("[USER - GET] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - GET] user Not have permission for this endpoint");
+      logger.error("[USER - GET] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER - GET] Error getting users from DB", e);
+      logger.error("[USER - GET] Error getting user {}", id, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -103,16 +109,18 @@ public class UserController {
   @ApiOperation(value = "Retrieve logged user data", response = User.class)
   public ResponseEntity<User> getUserLogged(Principal principal) {
     try {
-      logger.info("[USER - GET] Get user logged");
+      logger.info("[USER - LOGGED] Getting user logged {}", principal.getName());
 
       User user = userService.getUser(principal.getName());
 
+      logger
+          .info("[USER - LOGGED] Request for getting user logged {} finished", principal.getName());
       return new ResponseEntity<>(user, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
-      logger.error("[USER - GET] User not found", e);
+      logger.error("[USER - LOGGED] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found", e);
     } catch (Exception e) {
-      logger.error("[USER - GET] Error getting users from DB", e);
+      logger.error("[USER - LOGGED] Error getting user {}", principal.getName(), e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -123,26 +131,28 @@ public class UserController {
       @RequestParam(value = "page") int nPage,
       @RequestParam(value = "elements") int elements, Principal principal) {
     try {
-      logger.info("[USER - GET] Check user permission");
+      logger.info("[USER HOUSE - GET] Getting houses sensor {} list by user \"{}\"", id,
+          principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN) && !userService
           .isSameUser(principal.getName(), id)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER HOUSE - GET] Get user houses from DB with id: " + id);
 
       Page<House> page = userService.getHouses(id, nPage, elements);
 
+      logger.info("[USER HOUSE - GET] Request for getting houses sensor {} list by user \"{}\"", id,
+          principal.getName());
       return new ResponseEntity<>(page, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
-      logger.error("[USER - GET] User not found", e);
+      logger.error("[USER HOUSE - GET] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - GET] user Not have permission for this endpoint");
+      logger.error("[USER HOUSE - GET] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER - GET] Error getting users from DB", e);
+      logger.error("[USER HOUSE - GET] Error getting houses sensor {} list", id, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -151,25 +161,28 @@ public class UserController {
   @ApiOperation(value = "Register user", response = User.class)
   public ResponseEntity<User> register(@RequestBody() User user, Principal principal) {
     try {
-      logger.info("[USER - REGISTER] Check user permission");
+      logger
+          .info("[USER - REGISTER] Registering user {} by user \"{}\"", user, principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER - REGISTER] Registering user");
+
       user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
       User userRegister = userService.register(user);
 
+      logger.info("[USER - REGISTER] Request for registering user {} finished by user \"{}\"", user,
+          principal.getName());
       return new ResponseEntity<>(userRegister, HttpStatus.CREATED);
     } catch (EntityAlreadyExists e) {
       logger.error("[USER - REGISTER] user already exists", e);
       throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "User already exists", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - REGISTER] user Not have permission for this endpoint");
+      logger.error("[USER - REGISTER] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER - REGISTER] Error registering user", e);
+      logger.error("[USER - REGISTER] Error registering user {}", user, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -178,22 +191,24 @@ public class UserController {
   @ApiOperation(value = "Update user", response = User.class)
   public ResponseEntity<User> update(@RequestBody() User user, Principal principal) {
     try {
-      logger.info("[USER - UPDATE] Check user permission");
+      logger
+          .info("[USER - UPDATE] Updating user {} by user \"{}\"", user, principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN) && !userService
           .isSameUser(principal.getName(), user.getUsername())) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER - UPDATE] Registering user");
       user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
       User userRegister = userService.update(user);
 
+      logger.info("[USER - UPDATE] Request for updating user {} finished by user \"{}\"", user,
+          principal.getName());
       return new ResponseEntity<>(userRegister, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
       logger.error("[USER - UPDATE] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "User not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - REGISTER] user Not have permission for this endpoint");
+      logger.error("[USER - UPDATE] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
@@ -206,24 +221,26 @@ public class UserController {
   @ApiOperation(value = "Remove user", response = User.class)
   public ResponseEntity<User> remove(@PathVariable("id") long id, Principal principal) {
     try {
-      logger.info("[USER - REMOVE] Check user permission");
+      logger.info("[USER - REMOVE] Removing user {} by user \"{}\"", id, principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER - REMOVE] Removing user");
+
       User user = userService.remove(id);
 
+      logger.info("[USER - REMOVE] Request for removing user {} finished by user \"{}\"", id,
+          principal.getName());
       return new ResponseEntity<>(user, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
       logger.error("[USER - REMOVE] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "User not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER - REMOVE] user Not have permission for this endpoint");
+      logger.error("[USER - REMOVE] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER - REMOVE] Error removing user", e);
+      logger.error("[USER - REMOVE] Error removing user {}", id, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -233,23 +250,28 @@ public class UserController {
   public ResponseEntity<House> addUserHouseById(@PathVariable("id") long id,
       @PathVariable() long houseId, Principal principal) {
     try {
-      logger.info("[USER HOUSE - ADD] Check user permission");
+      logger.info("[USER HOUSE - ADD] Add house {} to user {} by user \"{}\"", houseId, id,
+          principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER HOUSE - ADD] Add new house to user");
+
       House house = userService.addHouse(id, houseId);
+
+      logger.info("[USER HOUSE - ADD] Request add house {} to user {} finished by user \"{}\"",
+          houseId, id,
+          principal.getName());
       return new ResponseEntity<>(house, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
       logger.error("[USER HOUSE - ADD] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "User or house not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER HOUSE - ADD] user Not have permission for this endpoint");
+      logger.error("[USER HOUSE - ADD] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER HOUSE - ADD] Error adding house", e);
+      logger.error("[USER HOUSE - ADD] Error adding house {} to user {}", houseId, id, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
@@ -259,23 +281,28 @@ public class UserController {
   public ResponseEntity<House> removeUserHouseById(@PathVariable("id") long id,
       @PathVariable() long houseId, Principal principal) {
     try {
-      logger.info("[USER HOUSE - REMOVE] Check user permission");
+      logger.info("[USER HOUSE - REMOVE] Removing house {} to user {} by user \"{}\"", houseId, id,
+          principal.getName());
       if (!userService.isUserAllowed(principal.getName(), Role.ADMIN)) {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN,
-            "user Not have permission for this endpoint");
+            "User does not have permission for this endpoint");
       }
-      logger.info("[USER HOUSE - REMOVE] Remove house to user");
+
+      logger.info(
+          "[USER HOUSE - REMOVE] Request removing house {} to user {} finished by user \"{}\"",
+          houseId, id,
+          principal.getName());
       House house = userService.removeHouse(id, houseId);
       return new ResponseEntity<>(house, HttpStatus.OK);
     } catch (EntityNotFoundException e) {
       logger.error("[USER HOUSE - REMOVE] User not found", e);
       throw new ResponseStatusException(HttpStatus.NOT_MODIFIED, "User or house not found", e);
     } catch (ResponseStatusException e) {
-      logger.error("[USER HOUSE - REMOVE] user Not have permission for this endpoint");
+      logger.error("[USER HOUSE - REMOVE] User does not have permission for this endpoint");
       throw new ResponseStatusException(HttpStatus.FORBIDDEN,
           e.getReason(), e);
     } catch (Exception e) {
-      logger.error("[USER HOUSE - REMOVE] Error removing house", e);
+      logger.error("[USER HOUSE - REMOVE] Error removing house {} to user {}", houseId, id, e);
       throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal error", e);
     }
   }
