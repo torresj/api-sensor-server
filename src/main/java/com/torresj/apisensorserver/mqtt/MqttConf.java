79bca1ae6bf1770824d31d1f5d730236f5fcaecf
Jaime Torres <jatorres@theworkshop.com>
2019-08-20 09:49:38 +0200

    New properties added

--- src/main/java/com/torresj/apisensorserver/mqtt/MqttConf.java
+++ src/main/java/com/torresj/apisensorserver/mqtt/MqttConf.java
@@ -1,7 +1,6 @@
 package com.torresj.apisensorserver.mqtt;
 
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,8 +20,11 @@ public class MqttConf {
   @Value("${spring.rabbitmq.host}")
   private String host;
 
-  @Value("${rabbitmq.mqtt.port}")
+  @Value("${spring.rabbitmq.mqtt.port}")
   private String port;
+
+  @Value("${spring.rabbitmq.mqtt.topic}")
+  private String topic;
 
   @Value("${spring.rabbitmq.username}")
   private String user;
@@ -55,7 +57,7 @@ public class MqttConf {
   public MessageProducerSupport mqttInbound() {
     MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(
         "mqttServer",
-        mqttClientFactory(), "mqtt/topic");
+        mqttClientFactory(), topic);
     adapter.setCompletionTimeout(5000);
     adapter.setConverter(new DefaultPahoMessageConverter());
     adapter.setQos(1);
