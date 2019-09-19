d003391fecf18a6a293707332cb4ce7ca548fc06
Jaime Torres <jtbenavente@gmail.com>
2019-09-20 01:09:46 +0200

    record date error fixed

--- src/main/java/com/torresj/apisensorserver/models/entities/Record.java
+++ src/main/java/com/torresj/apisensorserver/models/entities/Record.java
@@ -9,6 +9,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -38,6 +43,9 @@ public class Record implements Serializable {
   private double value;
 
   @Column(nullable = false, updatable = false)
+  @JsonSerialize(using = LocalDateTimeSerializer.class)
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  @JsonFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss'Z'")
   private LocalDateTime date;
 
   @Column(nullable = false, updatable = false)
