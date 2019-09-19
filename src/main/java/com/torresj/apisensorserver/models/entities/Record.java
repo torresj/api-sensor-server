cdd6bd42e9aebbf805e41e73fdaf821850d1e216
Jaime Torres <jtbenavente@gmail.com>
2019-09-20 01:00:07 +0200

    record date error fixed

--- src/main/java/com/torresj/apisensorserver/models/entities/Record.java
+++ src/main/java/com/torresj/apisensorserver/models/entities/Record.java
@@ -1,5 +1,6 @@
 package com.torresj.apisensorserver.models.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.time.LocalDateTime;
