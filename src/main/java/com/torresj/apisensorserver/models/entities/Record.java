2ab0e12fd8adecea0ae68fb9aa43fbba0fd3f01c
Jaime Torres <jatorres@theworkshop.com>
2019-09-20 11:57:25 +0200

    LocalDateTime format fixed

--- src/main/java/com/torresj/apisensorserver/models/entities/Record.java
+++ src/main/java/com/torresj/apisensorserver/models/entities/Record.java
@@ -1,6 +1,5 @@
 package com.torresj.apisensorserver.models.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -9,11 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -43,9 +37,6 @@ public class Record implements Serializable {
   private double value;
 
   @Column(nullable = false, updatable = false)
-  @JsonSerialize(using = LocalDateTimeSerializer.class)
-  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-  @JsonFormat(pattern = "yyyy-MM-dd'T'hh:mm:ss'Z'")
   private LocalDateTime date;
 
   @Column(nullable = false, updatable = false)
