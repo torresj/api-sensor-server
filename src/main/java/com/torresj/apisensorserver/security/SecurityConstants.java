3bf83d77ce94781465ad308eeaf6c1eefd5b032e
Jaime Torres <jatorres@theworkshop.com>
2019-10-17 14:48:27 +0200

    new JWT expiortaion time added to 24 hours

--- src/main/java/com/torresj/apisensorserver/security/SecurityConstants.java
+++ src/main/java/com/torresj/apisensorserver/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.torresj.apisensorserver.security;
 public class SecurityConstants {
 
   public static final String SECRET = "SecretKeyToGenJWTs";
-  public static final long EXPIRATION_TIME = 1_200_000; // 10 days
+  public static final long EXPIRATION_TIME = 86_400_000;
   public static final String TOKEN_PREFIX = "Bearer ";
   public static final String HEADER_STRING = "Authorization";
   public static final String SIGN_UP_URL = "/v1/users";
